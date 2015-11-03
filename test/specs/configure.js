f62bb66801eec27fb5e1da92e65e67fe74157ec8
Jonathon Kresner <jkresner@gmail.com>
2015-11-04 09:50:30 +1100

    Config / configure overhaul con tests
    + Add new defaults for github oauth
    + Solidify config concepts of defaults, overrides and environment vars
    + Print nice terminal feedback if config fails from missing settings

---
+++ test/specs/configure.js
@@ -0,0 +1,123 @@
+var Configure = require('../../lib/configure')
+
+
+module.exports = () => DESCRIBE("Config", function() {
+
+
+  beforeEach(function(){
+    for (var envVar in process.env) delete process.env[envVar]
+  })
+
+
+  IT('Default config fails with empty overrides', function() {
+    var fn = () => Configure('dev', {})
+    expect(fn).to.throw(Error, /Configure failed. Override or environment var required for config/)
+    DONE()
+  })
+
+
+  IT('Default config with barest env variables', function() {
+    process.env.AUTH_APPKEY = 'test2'
+    process.env.AUTH_OAUTH_GITHUB_CLIENTID = 'ghtest2'
+    process.env.AUTH_OAUTH_GITHUB_CLIENTSECRET = 'ghtest2-secret'
+    process.env.AUTH_OAUTH_GITHUB_USERAGENT = 'ghtest2-ua'
+    process.env.HTTP_SESSIONSTORE_COLLECTION = 'sessions-test2'
+    process.env.MONGOURL = 'mongo://ghtest2/db'
+    var conf2 = Configure('dev', {})
+    expect(conf2.auth.loginUrl).to.equal('/')
+    expect(conf2.auth.appKey).to.equal('test2')
+    expect(conf2.auth.oauth.github.short).to.equal('gh')
+    expect(conf2.auth.oauth.github.login).to.equal(true)
+    expect(conf2.auth.oauth.github.signup).to.equal(true)
+    expect(conf2.auth.oauth.github.emails).to.equal(true)
+    expect(conf2.auth.oauth.github.clientID).to.equal('ghtest2')
+    expect(conf2.auth.oauth.github.clientSecret).to.equal('ghtest2-secret')
+    expect(conf2.auth.oauth.github.userAgent).to.equal('ghtest2-ua')
+    expect(conf2.auth.oauth.github.callbackURL).to.equal('http://localhost:3333/auth/github/callback')
+    expect(conf2.auth.oauth.github.passReqToCallback).to.equal(true)
+    expect(conf2.auth.oauth.github.scope.length).to.equal(1)
+    expect(conf2.auth.oauth.github.scope[0]).to.equal('user')
+    expect(conf2.http.port).to.equal(3333)
+    expect(conf2.http.host).to.equal('http://localhost:3333')
+    expect(conf2.http.static.dir).to.equal('web/public')
+    expect(conf2.http.sessionStore.collection).to.equal('sessions-test2')
+    expect(conf2.mongoUrl).to.equal('mongo://ghtest2/db')
+    DONE()
+  })
+
+
+  IT('Undefine auth config section from overrides', function() {
+    var overrides3 = { auth: undefined }
+    process.env.HTTP_SESSIONSTORE_COLLECTION = 'sessions-test3'
+    process.env.MONGOURL = 'mongo://ghtest3/db'
+
+    var conf3 = Configure('dev', overrides3)
+    expect(conf3.auth).to.be.undefined
+    expect(conf3.http.host).to.equal('http://localhost:3333')
+    expect(conf3.http.sessionStore.collection).to.equal('sessions-test3')
+    expect(conf3.mongoUrl).to.equal('mongo://ghtest3/db')
+    DONE()
+  })
+
+
+  IT('Works with config logging on', function() {
+    var overrides0 = { auth: undefined }
+    process.env.LOG_CONF_THEME_LOAD = 'white'
+    process.env.HTTP_SESSIONSTORE_COLLECTION = 'sessions-test0'
+    process.env.MONGOURL = 'mongo://ghtest0/db'
+    var conf0 = Configure('dev', overrides0)
+    DONE()
+  })
+
+
+  IT('Applies override values on top of default config', function() {
+    var overrides4 = { auth: { oauth: { github: { signup: false } } }, mongoUrl: undefined }
+    process.env.AUTH_APPKEY = 'test4'
+    process.env.AUTH_OAUTH_GITHUB_CLIENTID = 'ghtest4'
+    process.env.AUTH_OAUTH_GITHUB_CLIENTSECRET = 'ghtest4-secret'
+    process.env.AUTH_OAUTH_GITHUB_USERAGENT = 'ghtest4-ua'
+    process.env.HTTP_SESSIONSTORE_COLLECTION = 'sessions-test4'
+    process.env.PORT = "4444"
+
+    var conf4 = Configure('dev', overrides4)
+    expect(conf4.auth.oauth.github.short).to.equal('gh')
+    expect(conf4.auth.oauth.github.login).to.equal(true)
+    expect(conf4.auth.oauth.github.signup).to.equal(false)
+    expect(conf4.auth.oauth.github.emails).to.equal(true)
+    expect(conf4.auth.oauth.github.clientID).to.equal('ghtest4')
+    expect(conf4.auth.oauth.github.clientSecret).to.equal('ghtest4-secret')
+    expect(conf4.auth.oauth.github.userAgent).to.equal('ghtest4-ua')
+    expect(conf4.auth.oauth.github.callbackURL).to.equal('http://localhost:4444/auth/github/callback')
+    expect(conf4.auth.oauth.github.passReqToCallback).to.equal(true)
+    expect(conf4.auth.oauth.github.scope.length).to.equal(1)
+    expect(conf4.auth.oauth.github.scope[0]).to.equal('user')
+    DONE()
+  })
+
+
+  IT('Add nested override where undefined default', function() {
+    var overrides5 = { log: { test5: { theme: { run: 'white', error: 'red' } } }, auth: undefined, mongoUrl: undefined }
+    process.env.HTTP_SESSIONSTORE_COLLECTION = 'sessions-test5'
+
+    var conf5 = Configure('dev', overrides5)
+    expect(conf5.log.test5.theme.run).to.equal('white')
+    expect(conf5.log.test5.theme.error).to.equal('red')
+    DONE()
+  })
+
+
+  IT('Applies env var on top of override', function() {
+    var overrides6 = { log: { test6: { theme: { run: 'blue', error: 'magentra' } } }, auth: undefined, mongoUrl: undefined }
+    process.env.HTTP_SESSIONSTORE_COLLECTION = 'sessions-test5'
+    process.env.LOG_TEST6_THEME_RUN = 'gray'
+    process.env.LOG_TEST6_THEME_ERROR = 'white'
+
+    var conf6 = Configure('dev', overrides6)
+    expect(conf6.log.test6.theme.run).to.equal('gray')
+    expect(conf6.log.test6.theme.error).to.equal('white')
+    DONE()
+
+  })
+
+
+})
